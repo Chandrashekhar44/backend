7bb646db9a13ecad3d4828d1c46f87abdc607c47
Chandra Shekhar <banothuchandu77@gmail.com>
2025-09-20 17:01:36 +0530

    fix: remove unused import statement from playlist.controller.js

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,7 +3,7 @@ import {Playlist} from "../models/playlist.model.js"
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
-import { use } from "react";
+
 
 const createPlaylist = asyncHandler(async(req,res)=>{
        const {name,description} = req.body
